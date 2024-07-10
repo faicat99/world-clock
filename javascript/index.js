1c7466318948c40faf6fc9b1b2d8a767b0104782
Fainche <faicatmck@gmail.com>
2024-07-10 15:13:58 +0100

    updated dropdown locations and added my current location

--- javascript/index.js
+++ javascript/index.js
@@ -31,6 +31,9 @@ londonTimeElement.innerHTML = londonTime.format("hh:mm:ss [<small>]A[</small>]")
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+        cityTimeZone = moment.tz.guess();
+    }
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
@@ -40,7 +43,7 @@ function updateCity(event) {
       <h2>${cityName}</h2>
       <div class="date">${cityTime.format("Do MMMM YYYY")}</div>
       </div>
-      <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+      <div class="time">${cityTime.format("hh:mm:ss")}<small>${cityTime.format(
         "A")}</small></div>
       </div>
       `;
