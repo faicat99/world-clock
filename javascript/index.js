a5a7594ca9b20d20d504eed9b65fefdb4245ad85
Fainche <faicatmck@gmail.com>
2024-07-10 14:40:49 +0100

    updated JS time and date elements for the drop down cities to display their info when selected

--- javascript/index.js
+++ javascript/index.js
@@ -2,6 +2,7 @@ setInterval(function () {
 //LA
 
 let laElement = document.querySelector("#la");
+if (laElement) {
 let laDateElement = laElement.querySelector(".date");
 let laTimeElement = laElement.querySelector(".time");
 let laTime = moment().tz("America/Los_Angeles");
@@ -9,16 +10,44 @@ let laTime = moment().tz("America/Los_Angeles");
 laDateElement.innerHTML = laTime.format("Do MMMM YYYY");
 laTimeElement.innerHTML = laTime.format("hh:mm:ss [<small>]A[</small>]");
 
+}
+
 
 
 //London
 
 let londonElement = document.querySelector("#london");
+if (londonElement) {
 let londonDateElement = londonElement.querySelector(".date");
 let londonTimeElement = londonElement.querySelector(".time");
 let londonTime = moment().tz("Europe/London");
 
 londonDateElement.innerHTML = londonTime.format("Do MMMM YYYY");
 londonTimeElement.innerHTML = londonTime.format("hh:mm:ss [<small>]A[</small>]");
+}
+}
+,1000);
 
-}, 1000);
+
+function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+    <div class="city">
+      <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("Do MMMM YYYY")}</div>
+      </div>
+      <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+        "A")}</small></div>
+      </div>
+      `;
+}
+
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change", updateCity);
+
